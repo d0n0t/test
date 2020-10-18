cac70847e0b714169ebd9121d3ef30dc35f6a6a3
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-18 23:29:02 +0530

    Update player.php

--- player.php
+++ player.php
@@ -1,4 +1,18 @@
+<?php
 
+$data=34;
+
+$encrypt_a=(($data*3245336234*473647)/945787);
+
+$link = "player.php?vid=".urlencode(base64_encode($encrypt_a);
+
+foreach (_$GET as $key=> $data)
+
+$data2=$_GET[$key] = base64_decode(urldecode($data)) ;
+
+echo $encrypt_b=((($data2*945787)/473647)/3245336234);
+}
+?>
 <title>PHOTOCALL.TV - Streaming</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -8,7 +22,7 @@
 <script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
 <div id="player"></div>
 <script>var player = new Clappr.Player({
-source: "<?php echo $_GET['$link'] ?>",
+source: "<?php echo $_GET['vid'] ?>",
 mimeType: "application/x-mpegURL",
 plugins: [LevelSelector, ChromecastPlugin],
 parentId: "#player",
